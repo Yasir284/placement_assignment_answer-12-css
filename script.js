01302079a54fe1bef65443f0b8560987270f78dc
Yasir284 <lambawalay@gmail.com>
2023-05-22 22:05:13 +0530

    completed the assignment

--- script.js
+++ script.js
@@ -11,13 +11,24 @@ let heroSectionSwiper = new Swiper(".hero-section-swiper", {
   },
 });
 
-let swiper = new Swiper(".mySwiper", {
-  slidePreview: 5,
-  spaceBetween: 30,
-  centeredSlides: true,
+let companyImageSlider = new Swiper(".company-image-slider", {
+  slidesPerView: 5,
+  spaceBetween: 40,
   loop: true,
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
+  },
+});
+
+let previewButtonSlider = new Swiper(".preview-button-slider", {
+  slidesPerView: 3,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
